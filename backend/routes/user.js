df0a65ef25dc56acd01d8908e95fb34e0fa28456
Szabó Tamás <szabotomika97@gmail.com>
2019-05-14 23:11:17 +0200

    user route and model added

---
+++ backend/routes/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const User=require('../models/user');
+const router = express.Router();
+
+router.post("/signup", (req,res, next) => {
+
+});
+
+module.exports = router;
