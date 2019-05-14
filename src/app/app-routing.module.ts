1fdec34543544278874b10548d15cd7a9008332b
Szabó Tamás <szabotomika97@gmail.com>
2019-05-14 22:14:00 +0200

    component moved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { MainComponent } from './main/main.component';
 import { GameComponent } from './main/game/game.component';
 
