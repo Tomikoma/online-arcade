3d9fdbe1ce87ad68c36273988f2215ee3450eb3f
Szabó Tamás <szabotomika97@gmail.com>
2019-05-14 20:47:17 +0200

    static data removed

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose")
+
 const Game=require('./models/game');
 
 const app=express();
 
-mongoose.connect("mongodb+srv://tom:MVwYEXXFzW1r5sVa@onlinearcade-1uy5h.mongodb.net/test?retryWrites=true",
+mongoose.connect("mongodb+srv://tom:MVwYEXXFzW1r5sVa@onlinearcade-1uy5h.mongodb.net/node-angular?retryWrites=true",
 {useNewUrlParser: true})
 .then(() => {
   console.log("Connected to database!");
@@ -14,41 +15,6 @@ mongoose.connect("mongodb+srv://tom:MVwYEXXFzW1r5sVa@onlinearcade-1uy5h.mongodb.
   console.log("Connection failed!");
 });
 
-games = [
-  {
-    id: '1',
-    title: 'Pac-man',
-    releaseDate: new Date(1980),
-    genre: 'Action',
-    description: 'Lorem ipsum balblablalbla',
-    modes: ['Single', 'Multi']
-  },
-  {
-    id: '2',
-    title: 'Pac-man2',
-    releaseDate: new Date(1985),
-    genre: 'Action',
-    description: 'Lorem ipsum balblablalbla2',
-    modes: ['Single', 'Multi']
-  },
-  {
-    id: '3',
-    title: 'Pac-man3',
-    releaseDate: new Date(1985),
-    genre: 'Action',
-    description: 'Lorem ipsum balblablalbla2',
-    modes: ['Single', 'Multi']
-  },
-  {
-    id: '4',
-    title: 'Pac-man4',
-    releaseDate: new Date(1985),
-    genre: 'Action',
-    description: 'Lorem ipsum balblablalbla2',
-    modes: ['Single', 'Multi']
-  },
-
-];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
@@ -66,10 +32,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/api/games", (req,res,next) => {
-  res.status(200).json({
-    message: "Posts fetched successfully",
-    games: games
+app.get("/api/games", (req, res, next) => {
+  Game.find().then(documents => {
+    res.status(200).json({
+      message: "Games fetched successfully!",
+      games: documents
+    });
   });
 });
 
